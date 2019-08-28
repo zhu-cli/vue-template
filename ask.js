9f7a7ac2cc915caffc0b5811083fb4236f3fcf48
fandonglai <704072802@qq.com>
2019-08-28 16:58:21 +0800

    1

---
+++ ask.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+      type: 'confirm',
+      name: 'private',
+      message: 'ths resgistery is private?',
+    },
+    {
+      type: 'input',
+      name: 'author',
+      message: 'author?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'description?',
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'license?',
+    },
+  ]
